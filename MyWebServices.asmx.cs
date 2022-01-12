cb303e02b3ae9e668b9b81c25d0954ba7d2e7190
ViceLL <72369834+ViceLL@users.noreply.github.com>
2022-01-11 21:20:15 -0400

    Add files via upload

---
+++ MyWebServices.asmx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace PracticaWebApp
+{
+    /// <summary>
+    /// Summary description for MyWebServices
+    /// </summary>
+    [WebService(Namespace = "http://practicasemana5INS368.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MyWebServices : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int CalcularAreaDeCuadrado(int x, int y)
+        {
+            return (x * y);
+        }
+        
+        [WebMethod]
+        public int CalcularAreaDeTriangulo(int x, int y)
+        {
+            return (x * y) / 2;
+        }
+
+        [WebMethod]
+        public int CalcularAreaDeCirculo(double r)
+        {
+            double PI = 3.1415926535897932;
+            return ((int)(PI * Math.Pow(r, 2)));
+        }
+    }
+   
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PracticaWebApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void validate_area_of_triangle_detect_correct_result()
+        {
+            //Arrange
+            int x = 12;
+            int y = 8;
+            int expected = 48;
+
+            //Act
+            double actual = MyWebServices.CalcularAreaDeTriangulo(x, y);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void validate_area_of_square_detect_correct_result()
+        {
+            //Arrange
+            int x = 4;
+            int y = 8;
+            int expected = 32;
+
+            //Act
+            int actual = MyWebServices.CalcularAreaDeCuadrado(x, y);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void validate_area_of_circle_detect_correct_result()
+        {
+            //Arrange
+            int r = 3;
+            int expected = 28;
+
+            //Act
+            int actual = MyWebServices.CalcularAreaDeCirculo(r);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+    }
+}
